6f9b77c564eec8dc847044220ea94f63fc0d49a3
allfa27 <160209025+allfa27@users.noreply.github.com>
2025-04-16 12:50:13 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -135,12 +135,6 @@ export default function AboutPage() {
               image: "https://images.unsplash.com/photo-1720345174544-65637a5307bf?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
             {
-              name: "Karan",
-              role: "Co-Founder & CTO",
-              bio: "Tech entrepreneur with a background in supply chain optimization",
-              image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            },
-            {
               name: "Palak Thakur",
               role: "Head of Farmer Relations",
               bio: "Third-generation farmer who understands the challenges farmers face",
